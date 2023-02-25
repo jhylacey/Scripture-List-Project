157ac82b0f6e628af416ca9736fb3623755f9cf3
jhylacey <jhylacey@proton.me>
2023-02-25 14:56:23 -0600

    Enhanced Add Item Function
    The function has been further developed but still isn't persistent which
    is the ultimate goal.

--- script.js
+++ script.js
@@ -11,20 +11,33 @@ function addItem(e) {
     if (newItem === '') {
         alert('Please add an item');
         return;
+        console.log(button);
     }
 
     // Create list item
     const li = document.createElement('li');
     li.appendChild(document.createTextNode(newItem));
-    console.log(li);
 
+    const button = createButton('remove-item btn-link text-red');
+    li.appendChild(button);
+
+    itemList.appendChild(li);
+
+    itemInput.value = '';
 }
 
 function createButton(classes) {
     const button = document.createElement('button');
     button.className = classes;
+    const icon = createIcon('fa-solid fa-xmark');
+    button.appendChild(icon);
     return button;
 }
 
+function createIcon(classes) {
+    const icon = document.createElement('i');
+    icon.className = classes;
+    return icon;
+}
 //Event Listeners
 itemForm.addEventListener('submit', addItem);
